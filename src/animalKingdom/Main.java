f5af6ad533ede208b4968589347b9bd591c64683
Annette Odingo <odingoannette@gmail.com>
2020-05-02 03:18:52 +0100

    Created collections for various animals in Main.java

--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -2,6 +2,23 @@ package animalKingdom;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hi");
+        Mammals panda =  new Mammals("Panda", 1869);
+        Mammals zebra = new Mammals("Zebra", 1778);
+        Mammals koala = new Mammals("Koala", 1816);
+        Mammals sloth = new Mammals("Sloath", 1804);
+        Mammals armadillo= new Mammals("Armadillo", 1978);
+        Mammals racoon= new Mammals("Racoon", 1758);
+        Mammals bigfoot= new Mammals("Bigfoot", 2021);
+
+
+        Birds pigeon= new Birds("Pigeon", 1837);
+        Birds peacock= new Birds("Peacock", 1821);
+        Birds toucan = new Birds("Toucan", 1758);
+        Birds parrot= new Birds("Parrot", 1824);
+        Birds swan= new Birds("Swan", 1758);
+
+        Fish salmon= new Fish("Salmon", 1758);
+        Fish catfish= new Fish("Catfish", 1817);
+        Fish perch= new Fish("Perch", 1758);
     }
 }
