23371b7b077d98545c21ac979857fc8aabca1b95
Annette Odingo <odingoannette@gmail.com>
2020-05-02 03:27:10 +0100

    Finalize on collections

--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -1,5 +1,7 @@
 package animalKingdom;
 
+import java.util.*;
+
 public class Main{
     public static void main(String[] args){
         Mammals panda =  new Mammals("Panda", 1869);
@@ -7,7 +9,7 @@ public class Main{
         Mammals koala = new Mammals("Koala", 1816);
         Mammals sloth = new Mammals("Sloath", 1804);
         Mammals armadillo= new Mammals("Armadillo", 1978);
-        Mammals racoon= new Mammals("Racoon", 1758);
+        Mammals raccoon= new Mammals("Raccoon", 1758);
         Mammals bigfoot= new Mammals("Bigfoot", 2021);
 
 
@@ -20,5 +22,23 @@ public class Main{
         Fish salmon= new Fish("Salmon", 1758);
         Fish catfish= new Fish("Catfish", 1817);
         Fish perch= new Fish("Perch", 1758);
+        ArrayList<AnimalAbstract> mammalList = new ArrayList<AnimalAbstract>();
+        mammalList.add(panda);
+        mammalList.add(zebra);
+        mammalList.add(koala);
+        mammalList.add(sloth);
+        mammalList.add(armadillo);
+        mammalList.add(raccoon);
+        mammalList.add(bigfoot);
+        ArrayList<AnimalAbstract> birdList = new ArrayList<AnimalAbstract>();
+        birdList.add(pigeon);
+        birdList.add(peacock);
+        birdList.add(toucan);
+        birdList.add(parrot);
+        birdList.add(swan);
+        ArrayList<AnimalAbstract> fishList = new ArrayList<AnimalAbstract>();
+        fishList.add(salmon);
+        fishList.add(catfish);
+        fishList.add(perch);
     }
 }
